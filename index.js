d23429cb54da8b21d834cd77c2e5403f3925b938
Terri <rterri84@yahoo.com>
2026-09-01 12:08:32 -0700

    Review

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function openMenu() {document.body.classList += " menu--open"}
-function closeMenu() {document.body. classlist.remove('menu--open')}
+function closeMenu() {document.body. classList.remove('menu--open')}
 
 
 
